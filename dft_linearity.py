b79f6341d72ef1353bc69316023810f2e4d87b29
umamaheswari-N <46396755+umamaheswari-N@users.noreply.github.com>
2019-03-22 14:19:52 +0530

    Add files via upload

---
+++ dft_linearity.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cmath as cm
+j=cm.sqrt(-1)
+def dft(x,N)
+	c=0
+	f=[]
+	for n in range(p)
+		for k in range(p)
+			c=c+(x[k]*np.exp((-(j*2*np.pi*k*l/N))))
+		r.append(c)
+		c=0
+	return r
+
